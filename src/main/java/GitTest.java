174a99990172a380afffc3318c7607bb75842da8
wsj <wsj@songsisi.com>
2025-10-17 19:03:01 +0800

    third commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -2,5 +2,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("测试切换版本");
+        System.out.println("第三次提交版本");
     }
 }
